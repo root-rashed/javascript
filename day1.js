43223356c4f92d4602a3db674db09a53f38e4cfc
rootrashed <rashedulislam.shajol@gmail>
2026-08-17 14:46:19 +0600

    update

--- day1.js
+++ day1.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+
+
+
+// Let declare a variable that can be changed later
+let name = "Rashed";
+let age = 23;
+
+console.log("My name is "+name+" and I am "+age+" years old.")
+
+
+
+
+// Const variable cannot be changed later
+const age1 = 23;
+console.log("I am "+age1+" years old")
