9396f957eb533c2e8bb0abefa1f67bc2280a7c60
rootrashed <rashedulislam.shajol@gmail>
2026-08-21 18:46:08 +0600

    update

---
+++ day6.js
@@ -0,0 +1,15 @@
+let name = "Rashed";
+let age = 22;
+const message = `Hello ${name}, you are ${age} years old`;
+
+console.log(message);
+
+
+
+
+
+
+
+const price = 100;
+const quantity = 3;
+console.log(`Total: ${price * quantity}`);
